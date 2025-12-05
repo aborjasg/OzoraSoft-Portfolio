00ac69b838fc75d9a31a7e7a575e63cb9e19eda9
Alex BG (Laptop) <aborjasg@gmail.com>
2025-12-05 00:22:14 -0700

    Add VideoCapture component [#5]

---
+++ OzoraSoft/OzoraSoft.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using IronOcr;
+
+namespace OzoraSoft.Web.Components.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        public string TextToShow = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+
+        protected void ReadOCR()
+        {
+            TextToShow = "Reading...";
+            var ocr = new IronTesseract();
+            using var input = new OcrInput();
+            input.LoadImage("C:\\Users\\aborj\\OneDrive\\Pictures\\BankEvidence-20251203.jpg");
+            var result = ocr.Read(input);
+            TextToShow  = result.Text;
+        }
+    }
+}
